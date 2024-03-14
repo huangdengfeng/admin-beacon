7e0013545ef80137d74a2936995c12d59bc6f55b
huangdengfeng <734839030@qq.com>
2024-03-14 09:49:13 +0800

    feat: add jvm mem

--- src/main/resources/setenv.sh
+++ src/main/resources/setenv.sh
@@ -12,7 +12,7 @@ if [ "${IN_CONTAINER}" = true ]; then
   JVM_MEM="-XX:+UseContainerSupport -XX:InitialRAMPercentage=10.0 -XX:MaxRAMPercentage=80.0"
 fi
 #JVM_DEBUG="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005"
-JVM_ARGS="-XX:AutoBoxCacheMax=2000 -XX:+UseStringDeduplication -XX:+UseG1GC -verbose:gc -Xlog:gc*,safepoint:${LOG_PATH}/gc.log:time,uptime:filecount=1,filesize=10M -XX:+HeapDumpOnOutOfMemoryError -XX:+ExitOnOutOfMemoryError -XX:HeapDumpPath=${LOG_PATH}/dump -XX:ErrorFile=${LOG_PATH}/hs_err_%p.log"
+JVM_ARGS="-XX:MaxDirectMemorySize=256m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=128m -XX:AutoBoxCacheMax=2000 -XX:+UseStringDeduplication -XX:+UseG1GC -verbose:gc -Xlog:gc*,safepoint:${LOG_PATH}/gc.log:time,uptime:filecount=1,filesize=10M -XX:+HeapDumpOnOutOfMemoryError -XX:+ExitOnOutOfMemoryError -XX:HeapDumpPath=${LOG_PATH}/dump -XX:ErrorFile=${LOG_PATH}/hs_err_%p.log"
 #优雅关机等待时间
 SHUTDOWN_SECONDS=30
 SERVER_OTPS="--spring.profiles.active=cli"
