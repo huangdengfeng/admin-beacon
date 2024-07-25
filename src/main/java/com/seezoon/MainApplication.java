d646fbdf3656043602735bce738fa18e87e14a12
dfenghuang <dfenghuang@tencent.com>
2024-07-25 15:09:04 +0800

    feat: add tx

--- src/main/java/com/seezoon/MainApplication.java
+++ src/main/java/com/seezoon/MainApplication.java
@@ -5,8 +5,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @MapperScan(basePackages = "com.seezoon.domain.dao.mapper")
+@EnableTransactionManagement
 @EnableConfigurationProperties({AppProperties.class})
 @SpringBootApplication
 public class MainApplication {
